0a26ee4063725478310161642bbd7924c49f6d00
weige <wgqin1@e.ntu.edu.sg>
2018-05-14 21:00:05 +0800

    micro

---
+++ 微服务翻译/img/Go microservices, part 2 - building our first service _ Callista Enterprise_files/disqus.js
@@ -0,0 +1,8 @@
+(function () {
+
+	var disqusShortname = 'callistablog';
+
+	var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+	dsq.src = '//' + disqusShortname + '.disqus.com/embed.js';
+	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
