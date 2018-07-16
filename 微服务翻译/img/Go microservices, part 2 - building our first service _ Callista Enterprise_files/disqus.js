540763816f9ddd577296b9a1577e2ba72c5f2af8
weige qin <wgqin@reddotpayment.com>
2018-07-16 14:59:07 +0800

    new

--- 微服务翻译/img/Go microservices, part 2 - building our first service _ Callista Enterprise_files/disqus.js
+++
@@ -1,8 +0,0 @@
-(function () {
-
-	var disqusShortname = 'callistablog';
-
-	var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-	dsq.src = '//' + disqusShortname + '.disqus.com/embed.js';
-	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
